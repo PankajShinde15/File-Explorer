9d8f757ab3661fb53efa8d8437222b0fd0064c96
Pankaj Shinde <103436613+PankajShinde15@users.noreply.github.com>
2023-08-15 18:41:05 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function checkFileName(event) {
-    const regex = /^(?!.{11,})[a-zA-Z0-9]*$/;
-    if (!fileName.match(regex)) {
-        window.alert("File name should be less than 10 char & it should not contain number!!")
-    }
-}
